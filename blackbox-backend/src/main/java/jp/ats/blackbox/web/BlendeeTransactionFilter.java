664594281c3c179aa9befc994f9118f8210eb929
ats.tchiba <ats.t.chiba@gmail.com>
2018-12-12 01:22:14 +0900

    update

---
+++ blackbox-backend/src/main/java/jp/ats/blackbox/web/BlendeeTransactionFilter.java
@@ -0,0 +1,82 @@
+package jp.ats.blackbox.web;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.blendee.jdbc.BlendeeManager;
+import org.blendee.jdbc.Transaction;
+import org.blendee.util.Blendee;
+import org.blendee.util.BlendeeConstants;
+
+/**
+ * {@link Filter} の範囲でトランザクションを管理するためのクラスです。
+ * @author 千葉 哲嗣
+ */
+public class BlendeeTransactionFilter implements Filter {
+
+	private static final ThreadLocal<Transaction> transaction = new ThreadLocal<>();
+
+	private static boolean blendeeStarted = false;
+
+	/**
+	 * 現在のトランザクションを返します。
+	 *
+	 * @return 現在のトランザクション
+	 */
+	public static Transaction transaction() {
+		return transaction.get();
+	}
+
+	private static synchronized void blendeeStarted() {
+		blendeeStarted = true;
+	}
+
+	public static boolean isBlendeeStarted() {
+		return blendeeStarted;
+	}
+
+	@Override
+	public void init(final FilterConfig config) throws ServletException {
+		Blendee.start(
+			Collections.list(config.getInitParameterNames())
+				.stream()
+				.collect(
+					Collectors.toConcurrentMap(
+						key -> BlendeeConstants.convert(key),
+						key -> BlendeeConstants.convert(key).parse(config.getInitParameter(key)))));
+
+		blendeeStarted();
+	}
+
+	@Override
+	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+		throws IOException, ServletException {
+		if (BlendeeManager.get().startsTransaction()) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		try {
+			Blendee.execute(t -> {
+				transaction.set(t);
+				chain.doFilter(request, response);
+			});
+		} catch (Throwable t) {
+			Throwable rootCause = ServletUtils.getRootCause(t);
+			throw new ServletException(rootCause);
+		} finally {
+			transaction.remove();
+		}
+	}
+
+	@Override
+	public void destroy() {}
+}
