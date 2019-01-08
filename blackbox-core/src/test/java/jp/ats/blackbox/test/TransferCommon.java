0207c92af5fc9fe0b2399ccd7bb3f3a1691c0e20
bitzer11 <ats.t.chiba@gmail.com>
2019-01-08 18:15:43 +0900

    update

--- blackbox-core/src/test/java/jp/ats/blackbox/test/TransferCommon.java
+++ blackbox-core/src/test/java/jp/ats/blackbox/test/TransferCommon.java
@@ -1,6 +1,5 @@
 package jp.ats.blackbox.test;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.blendee.jdbc.OptionKey;
@@ -24,18 +23,9 @@ public class TransferCommon {
 	}
 
 	private static Map<OptionKey<?>, Object> param() {
-		Map<OptionKey<?>, Object> param = new HashMap<>();
-		param.put(BlendeeConstants.SCHEMA_NAMES, new String[] { "bb" });
-		param.put(BlendeeConstants.LOG_STACKTRACE_FILTER, "^jp\\.ats\\.blackbox\\.");
-		param.put(BlendeeConstants.TABLE_FACADE_PACKAGE, "sqlassist");
-		param.put(BlendeeConstants.CAN_ADD_NEW_ENTRIES, true);
+		Map<OptionKey<?>, Object> param = Common.param();
 		param.put(BlendeeConstants.TRANSACTION_FACTORY_CLASS, TransferTestTransactionFactory.class);
-		param.put(BlendeeConstants.JDBC_DRIVER_CLASS_NAME, "org.postgresql.Driver");
-		param.put(BlendeeConstants.JDBC_URL, "jdbc:postgresql://192.168.193.128:5432/blackbox");
-		param.put(BlendeeConstants.JDBC_USER, "blackbox");
-		param.put(BlendeeConstants.JDBC_PASSWORD, "blackbox");
-		param.put(BlendeeConstants.HOME_STORAGE_IDENTIFIER, "blackbox-core");
-		//param.put(BlendeeConstants.AUTO_CLOSE_INTERVAL_MILLIS, 300);
+		param.put(BlendeeConstants.AUTO_CLOSE_INTERVAL_MILLIS, 0); //ﾃｽﾄ時はコネクションを閉じないので起動しない
 
 		return param;
 	}
